ba5c52a0a1808c57a73981b03166b1758475d222
qiaowu898 <55732873+qiaowu898@users.noreply.github.com>
2020-10-09 13:48:49 +0800

    This file contains some parameters we may use
    The parameters are mainly  about the dataset

---
+++ Parameters_Enum.py
@@ -0,0 +1,6 @@
+from enum import EnumMeta
+class parameters(EnumMeta):
+      TRAIN_FILE = 'train_17000.tsv'
+      TEST_FILE = 'test_7500.tsv'
+      TRAIN_DATA_NUM = 17000
+      TEST_DATA_NUM = 7500
